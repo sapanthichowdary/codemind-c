d90e9679e4d8424fc1e7b5b14b0f3b66a939a78b
sapanthichowdary <147791772+sapanthichowdary@users.noreply.github.com>
2023-10-24 10:36:59 +0530

    #include<stdio.h> int main() {     int x,y,z;     scanf("%d%d%d",&x,&y,&z);     z=2*(x+y)*z;     printf("%d",z); }

--- Frames.c
+++ Frames.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    int x,y,z,a;
-    scanf("%d%d%d%d",&x,&y,&z,&a);
-    a=2*(x+y)*z;
-    printf("%d",a);
+    int x,y,z;
+    scanf("%d%d%d",&x,&y,&z);
+    z=2*(x+y)*z;
+    printf("%d",z);
 }
